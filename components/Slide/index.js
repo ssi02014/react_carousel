27612772e35d530ced504e5b107832a5ee984797
JeonMinJae <ssi02014@naver.com>
2021-11-01 16:26:52 +0900

    Add: Slide Compnent

---
+++ components/Slide/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Image } from "./style";
+
+const Slice = ({ img }) => {
+  return (
+    <>
+      <Image src={img}></Image>
+    </>
+  );
+};
+
+export default Slice;
